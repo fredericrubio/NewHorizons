086c362f9c96e997acbc2f05376411fcdda08b77
fredericrubio <fredericrubio@192.168.0.15>
2018-09-28 22:42:29 +0200

    Rename UtilsLib files

---
+++ UtilsLib/Src/NHO_LOG.cpp
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   NHO_LOG.cpp
+ * Author: fredericrubio
+ * 
+ * Created on 21 septembre 2018, 22:12
+ */
+
+#include "NHO_LOG.hpp"
