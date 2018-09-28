283fb4fd3cebf9b5518c3183f3a76a0cd3e2c5e6
fredericrubio <fredericrubio@192.168.0.15>
2018-09-28 22:37:29 +0200

    .gitignore

---
+++ UtilsLib/Inc/NHO_Log.hpp
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   NHO_LOG.hpp
+ * Author: fredericrubio
+ *
+ * Created on 21 septembre 2018, 22:12
+ */
+
+#ifndef NHO_LOG_HPP
+#define NHO_LOG_HPP
+
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <sys/time.h>
+
+inline std::string NowTime();
+
+enum TLogLevel {logERROR, logWARNING, logINFO, logDEBUG, logDEBUG1, logDEBUG2, logDEBUG3, logDEBUG4};
+
+class NHO_LOG
+{
+public:
+    NHO_LOG();
+    virtual ~NHO_LOG();
+    std::ostringstream& Get(TLogLevel level = logINFO);
+public:
+    static TLogLevel& ReportingLevel();
+    static std::string ToString(TLogLevel level);
+    static TLogLevel FromString(const std::string& level);
+protected:
+    std::ostringstream os;
+private:
+    NHO_LOG(const NHO_LOG&);
+    NHO_LOG& operator =(const NHO_LOG&);
+};
+
+inline NHO_LOG::NHO_LOG()
+{
+}
+
+inline std::ostringstream& NHO_LOG::Get(TLogLevel level)
+{
+    os << "- " << NowTime();
+    os << " " << ToString(level) << ": ";
+    os << std::string(level > logDEBUG ? level - logDEBUG : 0, '\t');
+    return os;
+}
+
+inline NHO_LOG::~NHO_LOG()
+{
+    os << std::endl;
+    fprintf(stderr, "%s", os.str().c_str());
+    fflush(stderr);
+}
+
+inline TLogLevel& NHO_LOG::ReportingLevel()
+{
+    static TLogLevel reportingLevel = logDEBUG4;
+    return reportingLevel;
+}
+
+inline std::string NHO_LOG::ToString(TLogLevel level)
+{
+	static const char* const buffer[] = {"ERROR", "WARNING", "INFO", "DEBUG", "DEBUG1", "DEBUG2", "DEBUG3", "DEBUG4"};
+    return buffer[level];
+}
+
+inline TLogLevel NHO_LOG::FromString(const std::string& level)
+{
+    if (level == "DEBUG4")
+        return logDEBUG4;
+    if (level == "DEBUG3")
+        return logDEBUG3;
+    if (level == "DEBUG2")
+        return logDEBUG2;
+    if (level == "DEBUG1")
+        return logDEBUG1;
+    if (level == "DEBUG")
+        return logDEBUG;
+    if (level == "INFO")
+        return logINFO;
+    if (level == "WARNING")
+        return logWARNING;
+    if (level == "ERROR")
+        return logERROR;
+    NHO_LOG().Get(logWARNING) << "Unknown logging level '" << level << "'. Using INFO level as default.";
+    return logINFO;
+}
+
+typedef NHO_LOG NHO_FILELog;
+
+#define NHO_FILE_LOG(level) \
+    if (level > NHO_FILELog::ReportingLevel()) ; \
+    else NHO_LOG().Get(level)
+
+inline std::string NowTime()
+{
+    char buffer[11];
+    time_t t;
+    time(&t);
+    tm r = {0};
+    strftime(buffer, sizeof(buffer), "%X", localtime_r(&t, &r));
+    struct timeval tv;
+    gettimeofday(&tv, 0);
+    char result[100] = {0};
+    std::sprintf(result, "%s.%03ld", buffer, (long)tv.tv_usec / 1000); 
+    return result;
+}
+
+#endif /* NHO_LOG_HPP */
+
